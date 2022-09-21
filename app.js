8e6a9158f51211f28c8824c2151f6c3566a850ac
Arief Rahman <rahmanarief95@gmail.com>
2022-09-21 12:54:16 +0700

    Restart app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const koneksi = require("./config/database");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -14,25 +14,6 @@ app.get("/", (req, res) => {
     message : "Forbidden Area"
   });
 });
-
-
-app.get("/api/rest-card", (req, res) => {
-  const sqlQuery = "SELECT * FROM tbl_gi_kartu_istirahat WHERE deleted_at = '0'";
-
-  koneksi.query(sqlQuery, (err, rows, field) => {
-    if (err) {
-      return res.status(500).json({
-        message : "Query Error",
-        error : err
-      });
-    }
-
-    res.status(200).json({
-      success : true,
-      data : rows
-    });
-  });
-})
 
 
 app.listen(PORT, () => console.log(`Running Server at Port : ${PORT}`));
