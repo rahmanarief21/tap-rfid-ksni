4f48012a3e1f9a7c1c9447556c7b29d0764b7b38
Arief Rahman <rahmanarief95@gmail.com>
2022-10-26 17:45:17 +0700

    style : adding raspberry module to tesr routes

--- app/routes/test.routes.js
+++ app/routes/test.routes.js
@@ -3,7 +3,9 @@
 module.exports = app => {
 
 	const Karyawan = require("../controllers/karyawan.controller.js");
+	const Raspberry = require("../controllers/raspberry.controller.js");
 
 	app.get("/api/karyawan/:empId", Karyawan.getOneByNik);
 
+	app.get("/api/test2/:ip_address", Raspberry.getByIpAddress);
 };
