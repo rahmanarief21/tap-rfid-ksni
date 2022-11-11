c7e0a8ac4b40262d7cd4d0e41998804bbbf0f9f3
Arief Rahman <rahmanarief95@gmail.com>
2022-11-11 11:12:48 -0800

    bug: deleting test routes content

--- app/routes/test.routes.js
+++ app/routes/test.routes.js
@@ -2,10 +2,4 @@
 
 module.exports = app => {
 
-	const Karyawan = require("../controllers/karyawan.controller.js");
-	const Raspberry = require("../controllers/raspberry.controller.js");
-
-	app.get("/api/karyawan/:empId", Karyawan.getOneByNik);
-
-	app.get("/api/test2/:ip_address", Raspberry.getByIpAddress);
 };
